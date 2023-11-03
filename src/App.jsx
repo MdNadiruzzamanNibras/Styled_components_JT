3749ee9c5257a253a4109f36a45a464a6e9bb4ae
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-11-03 17:19:23 +0600

    sort by price

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,73 @@ import { Title } from "./Styled/Title";
 import { Image } from "./Styled/Image";
 import  watch from "./assets/Images/watch.jpg"
 import { Catagory } from "./Styled/Catagory";
+import { Select } from "./Styled/Setect";
 
 const App = () => {
   const [datas, setDatas] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedprice, setSelectedPrice] = useState("");
+  
   useEffect(() => {
     fetch('Product.json')
       .then(res => res.json())
     .then(data=> setDatas(data))
   }, [])
-  console.log(datas);
- const handleCategoryChange = (e) => {
-    console.log(e.target.value); 
+  
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value); 
+   
   };
+  const handlepriceChange = (e) => {
+    
+    setSelectedPrice(e.target.value)
+  };
+
+  const filteredData = datas
+  .filter((data) => {
+    if (selectedCategory === "") {
+      return true; 
+    } else {
+      return data.category === selectedCategory;
+    }
+  })
+    
+   const sortdata =filteredData.sort((a, b) => {
+    if (selectedprice === "lowprice") {
+      return a.price - b.price;
+    } else if (selectedprice === "highprice") {
+      return b.price - a.price;
+    } else {
+      return 0; 
+    }
+  });
+ 
+  
+  console.log(filteredData, "filterdata");
   return (
     <div>
 
       <div>
        <div>
-        <label>Select an option:</label>
-         <select name='catagory'  onChange={handleCategoryChange}>
+       
+         <Select name='catagory'  onChange={handleCategoryChange}>
             <option value="">All Categories</option>
             <option value="Men's Watches">Men's Watches</option>
             <option value="Women's Watches">Women's Watches</option>
             <option value="All Gender Watches">All Gender Watches</option>
-            <option value="Vintage Watches">Vintage Watches</option>
-          </select>
+           
+          </Select>
+        
+      </div>
+       <div>
+        
+         <Select name='price'  onChange={handlepriceChange}>
+            <option value="">None</option>
+            <option value="lowprice">Low to price</option>
+            <option value="highprice">High to price</option>
+            
+           
+          </Select>
         
       </div>
         <div>
@@ -38,12 +80,13 @@ const App = () => {
       </div>
       <GridContainer>
         {
-          datas.map(data =>
+          sortdata.map(data =>
             <DivGrid key={data.id}>
               <div >
                 <Image src={watch} alt="watch" />
                 <Title> {data.title}</Title>
                 <Catagory>{data.category}</Catagory>
+                <p>{ data.price}</p>
                 <p style={{width:'300px'}}>{ data.description}</p>
               </div>
               
---
+++ src/Styled/Setect.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+
+export const Select = styled.select`
+width: 200px;
+font-size: 1em;
+`
